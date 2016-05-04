9913ce800767d3f6e8f39b1a54c487de89a90342
Olivier Rheault Gagnon <ozz-18@hotmail.com>
2016-05-04 09:49:04 -0400

    Formulaire inscription mailling list

--- wp-content/themes/Signature/index.php
+++ wp-content/themes/Signature/index.php
@@ -52,9 +52,9 @@
 		<div class="modal-content">
 		  <div class="modal-header">
 			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			<h4 class="modal-title" id="myModalLabel">Subscribe to our mailing list</h4>
+			<h4 class="modal-title" id="myModalLabel">Abonnez-vous à notre liste de diffusion</h4>
 		  </div>
-		  <div class="modal-body">
+		  
 			<!-- Begin MailChimp Signup Form -->
 			<!--<link href="< ?php echo get_template_directory_uri(); ?>/assets/css/mailchimp-classic-10_7.css" rel="stylesheet" type="text/css">
 			< style type="text/css">
@@ -65,122 +65,129 @@
 			
 				<form action="//rets.us12.list-manage.com/subscribe/post?u=b2f21f8b320e7eea0e11ca760&amp;id=783d7babd4" method="post" id="mc-embedded-subscribe-form" 
 					name="mc-embedded-subscribe-form" class="validate form-horizontal" target="_blank" novalidate>
-					<div id="mc_embed_signup_scroll">
-						<div class="col-sm-12" style="color:red"><span class="asterisk">*</span> indicates required</div>
-						
-						<div class="form-group">
-							<label for="mce-EMAIL" class="col-sm-2 control-label">Email <span class="asterisk">*</span></label>
-							<div class="col-sm-10">
-							  <input type="email" class="form-control required email" id="inputEmail3" placeholder="Email">
+					<div class="modal-body">
+						<div id="mc_embed_signup_scroll">
+							<div class="col-sm-12 pull-right" style="color:red"><span class="asterisk">*</span> indicates required</div>
+							
+							<div class="form-group">
+								<label for="mce-EMAIL" class="col-sm-2 control-label">Email <span class="asterisk">*</span></label>
+								<div class="col-sm-10">
+								  <input type="email" class="form-control required email" name="EMAIL" id="mce-EMAIL" placeholder="Email">
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<label for="mce-FNAME" class="col-sm-2 control-label">Prenom</label>
-							<div class="col-sm-10">
-								<input type="text" value="" name="FNAME" class="form-control" id="mce-FNAME">
+							<div class="form-group">
+								<label for="mce-FNAME" class="col-sm-2 control-label">Prenom</label>
+								<div class="col-sm-10">
+									<input type="text" value="" name="FNAME" class="form-control" id="mce-FNAME">
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<label for="mce-LNAME" class="col-sm-2 control-label">Nom </label>
-							<div class="col-sm-10">
-								<input type="text" value="" name="LNAME" class="form-control" id="mce-LNAME">
+							<div class="form-group">
+								<label for="mce-LNAME" class="col-sm-2 control-label">Nom </label>
+								<div class="col-sm-10">
+									<input type="text" value="" name="LNAME" class="form-control" id="mce-LNAME">
+								</div>
 							</div>
-						</div>
-						<div class="col-sm-12">
-							<strong>Champs d'intérêt </strong>
-						</div>
-						<div class="checkbox">
-							<label for="mce-group[2965]-2965-0">
-								<input type="checkbox" value="1" name="group[2965][1]" id="mce-group[2965]-2965-0">
-								Réseautage
-							</label>
-						</div>	
-						<div class="checkbox">
-							<label for="mce-group[2965]-2965-1">
-								<input type="checkbox" value="2" name="group[2965][2]" id="mce-group[2965]-2965-1">
-								Entreprenariat
-							</label>
-						</div>					
-						<div class="checkbox">	
-							<label for="mce-group[2965]-2965-2">
-								<input type="checkbox" value="4" name="group[2965][4]" id="mce-group[2965]-2965-2">
-								Démarrage d'entreprise
-							</label>
-						</div>
-						<div class="checkbox">
-							<label for="mce-group[2965]-2965-3">
-								<input type="checkbox" value="8" name="group[2965][8]" id="mce-group[2965]-2965-3">
-								Événements organisés par le RETS
-							</label>						
-						</div>
-						<div class="checkbox">
-							<label for="mce-group[2965]-2965-4">
-								<input type="checkbox" value="16" name="group[2965][16]" id="mce-group[2965]-2965-4">
-								Entreprenariat social
-							</label>
-						</div>
-						
-						<div class="col-sm-12">
-							<strong>Status</strong>
-						</div>
-						<div class="checkbox">
-							<label for="mce-group[2969]-2969-0">
-								<input type="checkbox" value="32" name="group[2969][32]" id="mce-group[2969]-2969-0">
-								Professionnel
-							</label>
-						</div>
-						<div class="checkbox">
-							<input type="checkbox" value="64" name="group[2969][64]" id="mce-group[2969]-2969-1"><label for="mce-group[2969]-2969-1">Étudiant</label>
-						</div>
+							
+							<br />
+							<div class="col-sm-12">
+								<strong>Champs d'intérêt </strong>
+							</div>
+							<div class="checkbox">
+								<label for="mce-group[2965]-2965-0">
+									<input type="checkbox" value="1" name="group[2965][1]" id="mce-group[2965]-2965-0">
+									Réseautage
+								</label>
+							</div>	
+							<div class="checkbox">
+								<label for="mce-group[2965]-2965-1">
+									<input type="checkbox" value="2" name="group[2965][2]" id="mce-group[2965]-2965-1">
+									Entreprenariat
+								</label>
+							</div>					
+							<div class="checkbox">	
+								<label for="mce-group[2965]-2965-2">
+									<input type="checkbox" value="4" name="group[2965][4]" id="mce-group[2965]-2965-2">
+									Démarrage d'entreprise
+								</label>
+							</div>
+							<div class="checkbox">
+								<label for="mce-group[2965]-2965-3">
+									<input type="checkbox" value="8" name="group[2965][8]" id="mce-group[2965]-2965-3">
+									Événements organisés par le RETS
+								</label>						
+							</div>
+							<div class="checkbox">
+								<label for="mce-group[2965]-2965-4">
+									<input type="checkbox" value="16" name="group[2965][16]" id="mce-group[2965]-2965-4">
+									Entreprenariat social
+								</label>
+							</div>
+							
+							<br />
+							<div class="col-sm-12">
+								<strong>Status</strong>
+							</div>
+							<div class="checkbox">
+								<label for="mce-group[2969]-2969-0">
+									<input type="checkbox" value="32" name="group[2969][32]" id="mce-group[2969]-2969-0">
+									Professionnel
+								</label>
+							</div>
+							<div class="checkbox">
+								<label for="mce-group[2969]-2969-1">
+									<input type="checkbox" value="64" name="group[2969][64]" id="mce-group[2969]-2969-1">
+									Étudiant
+								</label>
+							</div>
+							<div class="checkbox">
+								<label for="mce-group[2969]-2969-2">
+									<input type="checkbox" value="128" name="group[2969][128]" id="mce-group[2969]-2969-2">
+									Autre
+								</label>
+							</div>
 
-						<div class="checkbox">
-							<input type="checkbox" value="128" name="group[2969][128]" id="mce-group[2969]-2969-2"><label for="mce-group[2969]-2969-2">Autre</label>
+						
+							<div id="mce-responses" class="clear">
+								<div class="response" id="mce-error-response" style="display:none"></div>
+								<div class="response" id="mce-success-response" style="display:none;color:green"></div>
+							</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+							<div style="position: absolute; left: -5000px;" aria-hidden="true">
+								<input type="text" name="b_b2f21f8b320e7eea0e11ca760_783d7babd4" tabindex="-1" value="">
+							</div>
 						</div>
-
-					
-						<div id="mce-responses" class="clear">
-							<div class="response" id="mce-error-response" style="display:none"></div>
-							<div class="response" id="mce-success-response" style="display:none"></div>
-						</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-						<div style="position: absolute; left: -5000px;" aria-hidden="true">
-							<input type="text" name="b_b2f21f8b320e7eea0e11ca760_783d7babd4" tabindex="-1" value="">
-						</div>
-						<div class="clear">						
-							<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" >
-						</div>
-					</div>
-				</form>		
-			<script type='text/javascript' src='<?php echo get_template_directory_uri(); ?>/assets/js/mailchimp-validate.js'></script>
-			
-			<script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text'; /*
-			 * Translated default messages for the $ validation plugin.
-			 * Locale: FR
-			 */
-			$.extend($.validator.messages, {
-					required: "Ce champ est requis.",
-					remote: "Veuillez remplir ce champ pour continuer.",
-					email: "Veuillez entrer une adresse email valide.",
-					url: "Veuillez entrer une URL valide.",
-					date: "Veuillez entrer une date valide.",
-					dateISO: "Veuillez entrer une date valide (ISO).",
-					number: "Veuillez entrer un nombre valide.",
-					digits: "Veuillez entrer (seulement) une valeur numérique.",
-					creditcard: "Veuillez entrer un numéro de carte de crédit valide.",
-					equalTo: "Veuillez entrer une nouvelle fois la même valeur.",
-					accept: "Veuillez entrer une valeur avec une extension valide.",
-					maxlength: $.validator.format("Veuillez ne pas entrer plus de {0} caractères."),
-					minlength: $.validator.format("Veuillez entrer au moins {0} caractères."),
-					rangelength: $.validator.format("Veuillez entrer entre {0} et {1} caractères."),
-					range: $.validator.format("Veuillez entrer une valeur entre {0} et {1}."),
-					max: $.validator.format("Veuillez entrer une valeur inférieure ou égale à {0}."),
-					min: $.validator.format("Veuillez entrer une valeur supérieure ou égale à {0}.")
-			});}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-			<!--End mc_embed_signup-->			
-		  </div>
-		  <div class="modal-footer">
-			<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-big-orange" >  <!-- data-dismiss="modal"-->
-			<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-		  </div>
+						
+				<script type='text/javascript' src='<?php echo get_template_directory_uri(); ?>/assets/js/mailchimp-validate.js'></script>
+				
+				<script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text'; /*
+				 * Translated default messages for the $ validation plugin.
+				 * Locale: FR
+				 */
+				$.extend($.validator.messages, {
+						required: "Ce champ est requis.",
+						remote: "Veuillez remplir ce champ pour continuer.",
+						email: "Veuillez entrer une adresse email valide.",
+						url: "Veuillez entrer une URL valide.",
+						date: "Veuillez entrer une date valide.",
+						dateISO: "Veuillez entrer une date valide (ISO).",
+						number: "Veuillez entrer un nombre valide.",
+						digits: "Veuillez entrer (seulement) une valeur numérique.",
+						creditcard: "Veuillez entrer un numéro de carte de crédit valide.",
+						equalTo: "Veuillez entrer une nouvelle fois la même valeur.",
+						accept: "Veuillez entrer une valeur avec une extension valide.",
+						maxlength: $.validator.format("Veuillez ne pas entrer plus de {0} caractères."),
+						minlength: $.validator.format("Veuillez entrer au moins {0} caractères."),
+						rangelength: $.validator.format("Veuillez entrer entre {0} et {1} caractères."),
+						range: $.validator.format("Veuillez entrer une valeur entre {0} et {1}."),
+						max: $.validator.format("Veuillez entrer une valeur inférieure ou égale à {0}."),
+						min: $.validator.format("Veuillez entrer une valeur supérieure ou égale à {0}.")
+				});}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+				<!--End mc_embed_signup-->			
+			  </div>
+			<div class="modal-footer">
+				<input type="submit" value="Fermer" name="subscribe" id="mc-embedded-subscribe" data-dismiss="modal" class="btn btn-default" >
+				<input type="submit" value="S'abonner" name="subscribe" id="mc-embedded-subscribe" class="btn btn-big-orange" >  <!-- data-dismiss="modal"-->
+			</div>
+		  </form>	
 		</div>
 	  </div>
 	</div>
